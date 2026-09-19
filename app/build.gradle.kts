89dc203428d1f08c84ad90d2bfaab98dbafeb11a
sperow3-wq <sperow3@gmail.com>
2026-09-19 23:04:50 +0200

    Build 0.8.5 cache-first caller ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "pl.usundlug.crmbridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.8.4"
+        versionCode = 13
+        versionName = "0.8.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
