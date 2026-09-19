7d2387cb9df670fc38d61297b658d3508675ff7e
sperow3-wq <sperow3@gmail.com>
2026-09-19 23:16:43 +0200

    Build 0.8.6 live status UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "pl.usundlug.crmbridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.8.5"
+        versionCode = 14
+        versionName = "0.8.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
