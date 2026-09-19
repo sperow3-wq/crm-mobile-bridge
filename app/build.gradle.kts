5c6b253557f9e9931a7445d2c9ff4bd81503cf64
sperow3-wq <sperow3@gmail.com>
2026-09-19 21:31:02 +0200

    Build 0.8.3 mobile bridge diagnostics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "pl.usundlug.crmbridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.8.2"
+        versionCode = 11
+        versionName = "0.8.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
