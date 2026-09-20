c639bd27ad10e1eafc64fb37655b1babb4f0ad51
sperow3-wq <sperow3@gmail.com>
2026-09-20 02:49:11 +0200

    Build 0.8.10 background call lifecycle fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "pl.usundlug.crmbridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.8.9"
+        versionCode = 18
+        versionName = "0.8.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
