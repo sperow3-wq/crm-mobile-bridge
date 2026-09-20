1de493b648b2fd2db65e292f51de1bb0bdce57e4
sperow3-wq <sperow3@gmail.com>
2026-09-20 03:21:32 +0200

    Build 0.8.11 custom CRM dialer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "pl.usundlug.crmbridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.8.10"
+        versionCode = 19
+        versionName = "0.8.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
